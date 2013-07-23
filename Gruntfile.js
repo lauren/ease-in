24f4a3177e675a06d21b826ba16b6eee2b1dece5
Lauren Sperber <lauren.sperber@gmail.com>
2013-07-23 13:32:01 -0400

    initial docs commit

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      development: {
+        options: {
+          compress: true
+        },
+        files: {
+          "build/ease-in.css": "src/ease-in.less",
+          "build/ease-in-docs.css": "src/ease-in-docs.less"
+        }
+      },
+      production: {
+        options: {
+          compress: true
+        },
+        files: {
+          "build/ease-in.css": "src/ease-in.less",
+          "build/ease-in-docs.css": "src/ease-in-docs.less"
+        }
+      }
+    },
+    watch: {
+      less: {
+        files: ['src/*.less'],
+        tasks: ['less'],
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+};
