d9d4d782d017d7a5a1687a636ddaafa19152e839
Lauren Sperber <lauren.sperber@gmail.com>
2013-07-30 15:51:13 -0400

    add fork me ribbon and fix error in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    less: {
       production: {
         options: {
           compress: true
